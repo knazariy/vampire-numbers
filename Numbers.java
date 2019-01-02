219b0cbc48971ee4ce833297988debae7fc369d9
knazariy <37501778+knazariy@users.noreply.github.com>
2019-01-02 23:25:41 +0200

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Numbers {
+    public static boolean isVampire(int n) {
+        List<Integer> digits = new LinkedList<>();  //List because digits can repeat
+        Set<Integer> combinations;                  //Set because we don't need repeated combs
+        int input = n;
+        int t = input;
+
+        if (input / 1000 < 1 || input / 1000 >= 10) //Only for 4-digit numbers
+            return false;
+
+        while (t > 0) {                             //Getting every digit
+            digits.add(t % 10);
+            t /= 10;
+        }
+
+        combinations = Combinations.get2DCombinations(digits);
+
+        //This is where magic happens (Checking for vampire numbers)
+        for (Integer i : combinations) {
+            for (Integer j : combinations) {
+                if (i * j == input) {
+                    if(isValidPair(i, j, digits)) {
+                        System.out.println(i + " * " + j + " = " + input);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    //DO NOT CHANGE
+    private static boolean isValidPair(int a, int b, List<Integer> digits) {
+        HashMap<Integer, Integer> usedDigits = new HashMap<>();
+        for(Integer i : digits) {
+            if(usedDigits.containsKey(i)) usedDigits.put(i, usedDigits.get(i)+1);
+            else usedDigits.put(i, 1);
+        }
+
+        int[] combDigits = new int[digits.size()];
+
+        int t;
+        int x = 0;
+        for(int i = 0; i < 2; i++) {
+            if(i==1) t = a;
+            else t = b;
+            while (t > 0) {
+                combDigits[x] = t % 10;
+                t /= 10;
+                ++x;
+            }
+        }
+
+        for(int i = 0; i < combDigits.length; i++) {
+            if (usedDigits.containsKey(combDigits[i])) {
+                usedDigits.put(combDigits[i], usedDigits.get(combDigits[i])-1);
+            }
+        }
+
+        for(int i = 0; i < 9; i++)
+            if (usedDigits.containsKey(i))
+                if (usedDigits.get(i) != 0) return false;
+
+        return true;
+    }
+}
--- Test.java
+++ Test.java
@@ -2,35 +2,9 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        Set<Integer> digits = new TreeSet<>();
-        Set<Integer> combinations;
-
-        Scanner sc = new Scanner(System.in);
-        int input;
-
-        System.out.println("Enter a 4-digit number: ");
-        input = sc.nextInt();
-        if (input / 1000 < 1) {
-            System.out.println("Number isn't 4-digit. Please, try again.");
-            return;
+        for(int i = 1000; i < 9999; i++) {
+            if(Numbers.isVampire(i)) System.out.println(i + " is a vampire number!");
         }
-        int t = input;
-        while(t > 0) {
-            digits.add(t % 10);
-            t /= 10;
-        }
-
-        combinations = Combinations.get2DCombinations(digits);
-
-        for(Integer i : combinations)
-            for(Integer j : combinations) {
-                if(i*j == input) {
-                    System.out.println(input + " is a vampire number!\n" +
-                            i + " * " + j + " = " + i * j);
-                    return;
-                }
-            }
-
+//        Numbers.isVampire(6880);
     }
-
 }
