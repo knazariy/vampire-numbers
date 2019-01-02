6f906533c07d8421dc9ee9ee207ae6df227dafdb
knazariy <37501778+knazariy@users.noreply.github.com>
2019-01-03 00:33:20 +0200

    Add files via upload
    • Fixed bugs
    • Changed collections for digits
    • Reduced size
    • Code from main method moved to 'Numbers' class

--- Combinations.java
+++ Combinations.java
@@ -1,20 +1,22 @@
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Combinations {
-
-    public static Set<Integer> get2DCombinations(Set<Integer> digits) {
+    public static Set<Integer> get2DCombinations(List<Integer> digits) {
         Set<Integer> combinations = new TreeSet<>();
 
-        int t;
-        for(Integer i : digits)
-            for(Integer j : digits) {
+        int t = 0;
+
+        for(Integer i : digits) {
+            for (Integer j : digits) {
+                if(i.equals(j)) continue;
                 t = i * 10 + j;
-                if(t/10>=1)
+                if (t / 10 >= 1)
                     combinations.add(t);
             }
+        }
 
         return combinations;
     }
-
 }
